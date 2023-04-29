e597a64bb3fff3e57edeb8131eeeabe6a87608e0
nemo <dahliahalesia8@gmail.com>
2023-04-29 15:23:44 +0630

    lop

--- src/components/musics/MusicCard.jsx
+++ src/components/musics/MusicCard.jsx
@@ -1,50 +1,113 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
+import { StateContextCustom } from '../../context/StateContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faForwardStep } from '@fortawesome/free-solid-svg-icons'
-import { faBackwardStep, faPlay, faRepeat, faShuffle } from '@fortawesome/free-solid-svg-icons'
+import { faForwardStep,
+        faBackwardStep, 
+        faPlay, 
+        faRepeat, 
+        faShuffle,
+        faPause
+      } from '@fortawesome/free-solid-svg-icons'
 
 const MusicCard = () => {
+  const{musics, isPlaying} = StateContextCustom(state => state)
+  console.log(musics);
+  const audioElement = useRef(null);
+
+  // useEffect(() => {
+  //   if (isPlaying) {
+  //     audioElement.current.play();
+  //   } else {
+  //     audioElement.current.pause();
+  //   }
+  // });
+
+  const skipSong = (forwards = true) => {
+    if (forwards) {
+      setCurrentSongIndex(() => {
+        let temp = currentSongIndex;
+        temp++;
+
+        if (temp > songs.length - 1) {
+          temp = 0;
+        }
+
+        return temp;
+      });
+    } else {
+      props.setCurrentSongIndex(() => {
+        let temp = props.currentSongIndex;
+        temp--;
+
+        if (temp < 0) {
+          temp = props.songs.length - 1;
+        }
+
+        return temp;
+      });
+    }
+  };
+
   return (
-    
-    <div className="card " style={{width: "18rem", backgroundColor: "pink", borderRadius:"18%", boxShadow:"8px 13px 7px gray, -1px -1px 9px white", border:"none"}}>
-      <div className='p-14'>
-        <img src="../../src/assets/music/cdDesign.jpg" className="card-img-top " 
-        style={{boxShadow:"2px 4px 3px 4px rgba(0, 0, 0, 0.2)", borderRadius:"12%",border:"10px solid lightpink"}} alt="..."/>
-        
-      </div> 
-      <div className="card-body flex justify-evenly">
-        <div className="mt-2 lg:mb-0 mb-6 flex items-center justify-center">
-          <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
-            style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
-            type="button">
-            <FontAwesomeIcon icon={faRepeat} />
-          </button>
-          <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
-            style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
-            type="button">
-               <FontAwesomeIcon icon={faBackwardStep} />
-          </button>
+    <>
+    {musics.map(music => {
+      return (
 
-          <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
-            style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
-            type="button">
-              <FontAwesomeIcon icon={faPlay} />
-          </button>
+        <div className="card " key={music.id} style={{width: "18rem",  backgroundColor: "pink", borderRadius:"18%", boxShadow:"8px 13px 7px gray, -1px -1px 9px white", border:"none"}}>
 
-          <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
-            style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
-            type="button">
-              <FontAwesomeIcon icon={faForwardStep} />
-          </button>
-          <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
-            style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
-            type="button">
-              <FontAwesomeIcon icon={faShuffle} />
-          </button>
+          <div className='px-14 pt-14 pb-4'>
+            <img src={music.img_src} className="card-img-top " 
+            style={{boxShadow:"2px 4px 3px 4px rgba(0, 0, 0, 0.2)", borderRadius:"12%",border:"10px solid lightpink", height: "10rem"}} alt="..."/>
+          </div> 
+
+          <div className='text-center'>
+            <h5 className=' font-bold'>{music.title}</h5>
+            <p className='text-sm font-semibold text-zinc-500'>{music.artist}</p>
+          </div>
+          <div>
+            <hr />
+          </div>
+
+          {/* MusicControl */}
+          <div className="card-body flex justify-evenly">
+            <div className="mt-2 lg:mb-0 mb-6 flex items-center justify-center">
+              
+              <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
+                style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
+                type="button">
+                <FontAwesomeIcon icon={faRepeat} />
+              </button>
+              
+              <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
+                style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
+                type="button" onClick={() => skipSong(false)}>
+                  <FontAwesomeIcon icon={faBackwardStep} />
+              </button>
+
+              <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
+                style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
+                type="button">
+                  <FontAwesomeIcon icon={isPlaying ? faPause : faPlay} />
+              </button>
+
+              <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
+                style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
+                type="button" onClick={() => skipSong()}>
+                  <FontAwesomeIcon icon={faForwardStep} />
+              </button>
+              <button className="bg-white font-normal h-8 w-10 items-center justify-center align-center rounded-full outline-none focus:outline-none mr-2 inline-block border-4 border-double border-orange-800" 
+                style={{boxShadow:"1px 1px 5px blue, 1px 1px 8px red"}}
+                type="button">
+                  <FontAwesomeIcon icon={faShuffle} />
+              </button>
+
+            </div>
+          </div>
         </div>
-        </div>
-    </div>
-   
+      )
+      })
+    }
+    </>
    
   )
 }
--- src/context/StateContext.jsx
+++ src/context/StateContext.jsx
@@ -4,30 +4,34 @@ const StateContext = createContext();
 
 export const StateContextProvider = ({children}) => {
 
-    const [musics,setMusics] = useState([
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [musics, setMusics] = useState([
         {
+            "id": 1,
             "title": "Cupid",
             "artist": "Fifty Fifty",
             "album": " Cupido",
             "track": "Cupid",
             "year": "2023",
-            "img_src": "./.jpg",
-            "src": "./.mp3"
+            "img_src": "../../src/assets/image/trackOne.jpg",
+            "src": "../../src/assets/music/pastLives.mp3"
         },
         {
+            "id": 2,
             "title": "Cupid",
             "artist": "Fifty Fifty",
             "album": " Cupido",
             "track": "Cupid",
             "year": "2023",
-            "img_src": "./.jpg",
-            "src": "./.mp3"
+            "img_src": "../../src/assets/image/trackTwo.jpg",
+            "src": "../../src/assets/music/cupidTwin.mp3"
         }
     ])
-    const data = {musics, setMusics}
+    const data = {musics, setMusics, isPlaying, setIsPlaying}
     return (
         <StateContext.Provider value={data}>
             {children}
         </StateContext.Provider>
     )
 }
+export const StateContextCustom = () => useContext(StateContext)
