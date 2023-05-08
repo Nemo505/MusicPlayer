3f1aba4a78ae9a5d6d12304d5119d3021c5c6734
nemo <dahliahalesia8@gmail.com>
2023-05-09 01:03:37 +0630

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,20 @@ import React from 'react'
 const Footer = () => {
   return (
 
-    <div>Footer</div>
+    <footer class=" " style={{background: "linear-gradient(to right bottom, #FFA49F,#1ae5ec)", padding:"10px"}}>
+      <div class="container-fluid text-white">
+          <div class="row">
+              <div class="col-6">
+                  <script>document.write(new Date().getFullYear())</script> © SCN
+              </div>
+              <div class="col-6">
+                  <div class="text-sm-end d-none d-sm-block">
+                      Designed & Developed by <a href="" class="text-decoration-underline text-black" target="_blank">Kazue</a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </footer>
   )
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-      <nav className="navbar" style={{background: "linear-gradient(to right bottom, #FFA49F,#1ae5ec)"}}>
+      <nav className="navbar" style={{background: "linear-gradient(to right bottom, #FFA49F,#1ae5ec)", width:"100%", position:"fixed"}}>
         <a className="navbar-brand" href="#">
           <img src="../../src/assets/image/logo.png"  className="d-inline-block align-top" height={30} width={30} alt=""/>
           Maple Player
