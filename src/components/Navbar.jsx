90b54221ce7ef9e1ceda0f406944c80d7f03717e
nemo <dahliahalesia8@gmail.com>
2023-06-07 02:53:37 +0630

    'logo'

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ const Navbar = () => {
   return (
       <nav className="navbar" style={{background: "linear-gradient(to right bottom, #FFA49F,#1ae5ec)", width:"100%", position:"fixed"}}>
         <a className="navbar-brand mx-3" href="#">
-          <img src="../../src/assets/image/logo.png"  className="d-inline-block align-top me-3" height={30} width={30} alt=""/>
+          <img src="./image/logo.png"  className="d-inline-block align-top me-3" height={30} width={30} alt=""/>
           Maple Player
         </a>
       </nav>
